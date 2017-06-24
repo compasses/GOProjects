ecc10c61ac75c14276638671e1383441d664f8a5
Jet He <jet.he@sap.com>
2017-06-24 16:14:03 +0800

    new code

--- RoutineExamples/DHTSpider.go
+++ RoutineExamples/DHTSpider.go
@@ -14,11 +14,13 @@ import (
 	"os"
 	"runtime"
 	"time"
+	"sync/atomic"
+	"math"
 )
 
 // refer :https://github.com/jyootai/DhtBT/
 
-var BOOTSTRAP []string = []string {
+var BOOTSTRAP []string = []string{
 	"67.215.246.10:6881",  //router.bittorrent.com
 	"91.121.59.153:6881",  //dht.transmissionbt.com
 	"82.221.103.244:6881", //router.utorrent.com
@@ -81,7 +83,87 @@ type Response struct {
 func (dhtNode *KNode) Run() {
 	dhtNode.log.Println(fmt.Sprintf("Current Node ID is %s ", dhtNode.node.id))
 	dhtNode.log.Println(fmt.Sprintf("DhtBT %s is runing...", dhtNode.network.conn.LocalAddr().String()))
+	go func() { dhtNode.network.GetInfohash()}()
 
+	go func() { dhtNode.FindNode()}()
+}
+
+func (dhtNode *KNode) FindNode() {
+	for {
+		if dhtNode.routing.table[0].Len() == 0{
+			dhtNode.searchNodes(dhtNode.node.id)
+		} else {
+			for _, node := range dhtNode.routing.table[0].nodes {
+				t := time.Now()
+				d, _ := time.ParseDuration("-10s")
+				last := t.Add(d)
+				ok := node.lastSeen.Before(last)
+				if ok {
+					continue
+				}
+				dhtNode.GoFindNode( node, GenerateId())
+			}
+			dhtNode.routing.table[0].nodes = nil
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
+func (dhtNode *KNode) searchNodes(target Id) {
+	for _, host := range BOOTSTRAP {
+		addr, err := net.ResolveUDPAddr("udp", host)
+		if err != nil {
+			dhtNode.log.Fatalf("Resolve DNS error, %s\n", err)
+			return
+		}
+		node := new(NodeInfo)
+		node.port = addr.Port
+		node.ip = addr.IP
+		dhtNode.GoFindNode(node, target)
+	}
+}
+
+func (dhtNode *KNode) GoFindNode(info *NodeInfo, target Id){
+	if info.ip.Equal(net.IPv4(0, 0, 0, 0)) || info.port == 0 {
+		return
+	}
+	addr := new(net.UDPAddr)
+	addr.IP = info.ip
+	addr.Port = info.port
+	data, err := dhtNode.krpc.EncodingFindNode(target)
+	if err != nil {
+		dhtNode.log.Println(err)
+		return
+	}
+	err = dhtNode.network.Send([]byte(data), addr)
+	if err != nil {
+		dhtNode.log.Println(err)
+		return
+	}
+}
+
+func (krpc *KRPC) EncodingFindNode(target Id) (string, error)  {
+	tid := krpc.GenTID()
+	v := make(map[string]interface{})
+	v["t"] = fmt.Sprintf("%d", tid)
+	v["y"] = "q"
+	v["q"] = "find_node"
+	args := make(map[string]string)
+	args["id"] = string(krpc.dhtNode.node.id)
+	args["target"] = string(target) //查找自己，找到离自己较近的节点
+	v["a"] = args
+	s, err := bencode.EncodeString(v)
+	if err != nil {
+		krpc.dhtNode.log.Fatalln(err)
+	}
+	return s, err
+}
+
+func (encode *KRPC) GenTID() uint32 {
+	return encode.autoID() % math.MaxUint16
+}
+
+func (encode *KRPC) autoID() uint32 {
+	return atomic.AddUint32(&encode.tid, 1)
 }
 
 func (network *Network) GetInfohash() {
@@ -159,7 +241,7 @@ func (table *Bucket) Add(n *NodeInfo) {
 	table.Updatetime(n)
 }
 
-func (bucket *Bucket) Updatetime(n *NodeInfo ) {
+func (bucket *Bucket) Updatetime(n *NodeInfo) {
 	bucket.lastChange = time.Now()
 	n.lastSeen = time.Now()
 }
@@ -184,7 +266,6 @@ func (id Id) CompareTo(other Id) int {
 	}
 }
 
-
 func ParseBytesStream(data []byte) []*NodeInfo {
 	var nodes []*NodeInfo = nil
 	for j := 0; j < len(data); j = j + 26 {
@@ -205,8 +286,65 @@ func ParseBytesStream(data []byte) []*NodeInfo {
 func (krpc *KRPC) Query(msg *KRPCMSG) {
 	if query, ok := msg.Args.(*Query); ok {
 		queryNode := new(NodeInfo)
-
+		queryNode.ip = msg.addr.IP
+		queryNode.port = msg.addr.Port
+		queryNode.id = Id(query.A["id"].(string))
+		switch query.Q {
+		case "find_noe":
+			closeNodes := krpc.dhtNode.routing.table[1].nodes
+			nodes := ConvertByteStream(closeNodes)
+			data, _ := krpc.EncodingNodeResult(msg.T, "", nodes)
+			krpc.dhtNode.network.Send([]byte(data), msg.addr)
+		case "announce_peer":
+			if infohash, ok := query.A["info_hash"].(string); ok {
+				krpc.dhtNode.outChan <- Id(infohash).String()
+			}
+		case "get_peers":
+			if infohash, ok := query.A["info_hash"].(string); ok {
+				krpc.dhtNode.outChan <- Id(infohash).String()
+				token := krpc.dhtNode.GenToken(queryNode)
+				nodes := ConvertByteStream(krpc.dhtNode.routing.table[1].nodes)
+				data, _ := krpc.EncodingNodeResult(msg.T, token, nodes)
+				krpc.dhtNode.network.Send([]byte(data), msg.addr)
+			}
+		}
+		krpc.dhtNode.routing.InsertNode(queryNode)
 	}
+}
+
+func (knode *KNode)GenToken(sender *NodeInfo) string {
+	h := sha1.New()
+	io.WriteString(h, sender.ip.String())
+	io.WriteString(h, time.Now().String())
+	token := bytes.NewBuffer(h.Sum(nil)).String()
+	return token
+}
+
+func (network *Network)Send(data []byte, addr *net.UDPAddr) error {
+	_, err := network.conn.WriteToUDP(data, addr)
+	if err != nil {
+		network.dhtNode.log.Println("error send ", err)
+	}
+	return err
+}
+
+func ConvertByteStream(nodes []*NodeInfo) []byte {
+	bf := bytes.NewBuffer(nil)
+	for _, v := range nodes {
+		convertNodeInfo(bf, v)
+	}
+	return bf.Bytes()
+}
+
+func convertNodeInfo(buf *bytes.Buffer, v *NodeInfo) {
+	buf.Write(v.id)
+	convertIPPort(buf, v.ip, v.port)
+}
+
+func convertIPPort(buf *bytes.Buffer, ip net.IP, port int) {
+	buf.Write(ip.To4())
+	buf.WriteByte(byte((port&0xFF00) >> 8))
+	buf.WriteByte(byte(port&0xFF));
 }
 
 func (krpc *KRPC) EncodingNodeResult(tid string, token string, nodes []byte) (string, error) {
@@ -332,6 +470,5 @@ func NewKrpc(dhtNode *KNode) *KRPC {
 
 func Executing(cpu int, master chan string) {
 	dhtNode := NewDHTNode(master, os.Stdout)
-	dhtNode.node = NewNodeInfo()
-
+	dhtNode.Run()
 }
